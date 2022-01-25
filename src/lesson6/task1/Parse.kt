ae7a0d56609768bdcbd4e4dede4ba28d5a1b2811
skiterskater <egorozhegoc@yandex.ru>
2022-01-25 16:35:01 +0300

    fixes 6

--- src/lesson6/task1/Parse.kt
+++ src/lesson6/task1/Parse.kt
@@ -267,19 +267,21 @@ fun firstDuplicateIndex(str: String): Int = TODO()
  * Все цены должны быть больше нуля либо равны нулю.
  */
 fun mostExpensive(description: String): String {
-    println(" $description;")
-    // if (description.isEmpty() || !((" $description").matches(Regex("""(\s[а-яА-Я]+\s\d+\.\d;)+""")))) return ""
-    val product = description.replace(";", "").split(" ")
-    // переводим нашу строку в список вида: [название товара, цена, название товара, цена и тд.]
-    var max = -1.0
-    var maxName = ""
-    for (i in product.indices step 2) {
-        if (product[i + 1].toDouble() > max) {
-            max = product[i + 1].toDouble()
-            maxName = product[i]
+    val str = " $description;"
+    return if (description.isNotEmpty() && (str.matches(Regex("""(\s[а-яА-Я]+\s\d+\.\d;)+""")))) {
+        val product = description.replace(";", "").split(" ")
+        println(product)
+        // переводим нашу строку в список вида: [название товара, цена, название товара, цена и тд.]
+        var max = 0.0
+        var maxName = ""
+        for (i in product.indices step 2) {
+            if (product[i + 1].toDouble() > max) {
+                max = product[i + 1].toDouble()
+                maxName = product[i]
+            }
         }
-    }
-    return maxName
+        maxName
+    } else ""
 }
 
 /**
